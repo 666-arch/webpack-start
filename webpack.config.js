02827c470417102b0c79cf71969c89e97ae40665
MaxHuang <hc18174419502@163.com>
2025-06-19 14:42:55 +0800

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     mode: 'development', //默认值 production
+    devtool: 'inline-source-map', //方便查看打包后的源代码
     entry: './src/index.js', //指定打包执行流程入口文件从何开始
     output: {
         filename: 'dist.js', //指定输出的打包目录名称
@@ -22,6 +23,16 @@ module.exports = {
             {
                 test: /\.(png|svg|jpeg|jpg|gif)$/i, //webpack有原生支持图片的处理器，无需另外添加loader
                 type: 'asset/resource'
+            },
+            {
+                test: /\.js$/, //通过babel loader编译兼容大部分浏览器可解析的js代码
+                exclude: /node_modules/, //忽略node_mudules 目录 因为我们不需要转义node_modules下的代码
+                use: { //指定你通过什么loader转义
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
             }
         ]
     }
