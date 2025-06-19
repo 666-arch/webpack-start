b5d8fc8bc969b8a38a3c9b8b3b9c0f70fb1e5d53
MaxHuang <hc18174419502@163.com>
2025-06-19 10:18:50 +0800

    css loader config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 const { getBlogPost } = require("./data");
 console.log("hello world");
 console.log(getBlogPost())
+import "./style.css"
+const blogs = getBlogPost();
+const ul = document.createElement('ul');
+blogs.forEach(blog=>{
+    const li = document.createElement('li');
+    li.innerText = blog
+    ul.appendChild(li)
+})
+document.body.appendChild(ul)
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,13 @@ module.exports = {
     output: {
         filename: 'dist.js', //指定输出的打包目录名称
         path: path.resolve(__dirname, 'dist') //resolve可以指定多级目录，指定最终打包到dist目录下
+    },
+    module: {
+        rules: [ //这里就是你需要去匹配什么样的loader
+            {
+                test: /\.css$/i, //通过正则去匹配所有以 css 结尾的文件 （css预处理器同理）
+                use: ["style-loader", "css-loader"], //使用css相关的loader
+            }
+        ]
     }
 }
